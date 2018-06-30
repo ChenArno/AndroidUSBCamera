013791670f1d42daf661ddfee0f8bb740d95eee8
ChenArno <517625126@qq.com>
2018-06-30 13:32:58 +0800

    修改拍照方法

--- libusbcamera/src/main/java/com/serenegiant/usb/common/AbstractUVCCameraHandler.java
+++ libusbcamera/src/main/java/com/serenegiant/usb/common/AbstractUVCCameraHandler.java
@@ -363,8 +363,8 @@ public abstract class AbstractUVCCameraHandler extends Handler {
                 thread.handleStopPreview();
                 break;
             case MSG_CAPTURE_STILL:
-//				thread.handleCaptureStill((String)msg.obj);
-                thread.handleStillPicture((String) msg.obj);
+				thread.handleCaptureStill((String)msg.obj);
+                // thread.handleStillPicture((String) msg.obj);
                 break;
             case MSG_CAPTURE_START:
 //			thread.handleStartRecording((String)msg.obj);
